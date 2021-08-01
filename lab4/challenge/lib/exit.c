0f4d10760f2562d83009c88c019aee17a269303f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-01 16:30:25 +0800

    Delete lab4 directory

--- lab4/challenge/lib/exit.c
+++
@@ -1,9 +0,0 @@
-
-#include <inc/lib.h>
-
-void
-exit(void)
-{
-	sys_env_destroy(0);
-}
-
--- lab4/partA/init.d
+++
@@ -1,3 +0,0 @@
-init.o: kern/init.c inc/stdio.h inc/stdarg.h inc/string.h inc/types.h \
- inc/assert.h kern/monitor.h kern/console.h kern/pmap.h inc/memlayout.h \
- inc/mmu.h kern/kclock.h kern/env.h inc/env.h inc/trap.h kern/trap.h
